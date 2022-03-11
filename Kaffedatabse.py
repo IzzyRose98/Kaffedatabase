2936ceaacedbba81c026f5f055ec4c58f012af5f
IzzyRose98 <69513640+IzzyRose98@users.noreply.github.com>
2022-03-11 09:56:49 +0100

    Added file via upload

---
+++ Kaffedatabse.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+con = sqlite3.connect("Kaffe.db")
+cursor = con.cursor()
+cursor.execute('CREATE TABLE Bruker (Epost TEXT PRIMARY KEY NOT NULL UNIQUE, Passord TEXT NOT NULL, Fullt navn TEXT NOT NULL)')
+cursor.execute('CREATE TABLE Lokasjon (LokasjonsID NUMERIC PRIMARY KEY NOT NULL UNIQUE, Land TEXT NOT NULL, Region TEXT NOT NULL)')
+cursor.execute('CREATE TABLE Gård (GårdsID NUMERIC PRIMARY KEY NOT NULL UNIQUE, Moh INTEGER, Beskrivelse TEXT)')
+cursor.execute('CREATE TABLE Foredlingsmetode (ForedlingsmetodeID NUMERIC PRIMARY KEY NOT NULL UNIQUE, Navn TEXT, Beskrivelse TEXT)')
+cursor.execute('CREATE TABLE Kaffeparti (KaffepartiID TEXT PRIMARY KEY NOT NULL UNIQUE, Innhøstingsår INTEGER, Betalt INTEGER, GårdsID INTEGER NOT NULL, ForedlingsmetodeID INTEGER NOT NULL, FOREIGN KEY(GårdsID) REFERENCES Gård(GårdsID), FOREIGN KEY (ForedlingsmetodeID) REFERENCES Foredlingsmetode(ForedlingsmetodeID))')
+cursor.execute('CREATE TABLE FerdigbrentKaffe (FerdigbrentkaffeID NUMERIC PRIMARY KEY NOT NULL UNIQUE, Dato INTEGER, Navn TEXT, Beskrivelse TEXT, Kilopris INTEGER, Brenningsgrad TEXT, KaffepartiID INTEGER NOT NULL, FOREIGN KEY(KaffepartiID) REFERENCES Kaffeparti(KaffepartiID))')
+cursor.execute('CREATE TABLE Kaffesmaking (KaffesmakingID NUMERIC PRIMARY KEY NOT NULL UNIQUE, Notat TEXT, Poeng INTEGER, Smaksdato INTEGER, FerdigbrentKaffeID INTEGER NOT NULL, Epost TEXT, FOREIGN KEY(FerdigbrentKaffeID) REFERENCES FerdigbrentKaffe(FerdigbrentKaffeID), FOREIGN KEY(Epost) REFERENCES Bruker(Epost))')
+cursor.execute('CREATE TABLE Brenneri (BrenneriID INTEGER PRIMARY KEY, Navn TEXT )')
+cursor.execute('CREATE TABLE Kaffebønner (Art TEXT PRIMARY KEY NOT NULL UNIQUE)')
+cursor.execute('CREATE TABLE Dyrking (id NUMERIX PRIMARY KEY NOT NULL UNIQUE, Art TEXT NOT NULL, GårdsID NUMERIC NOT NULL)')
+cursor.execute('CREATE TABLE InnholdKaffeParti (id INTEGER PRIMARY KEY NOT NULL UNIQUE, Art TEXT NOT NULL, KaffepartiID NUMERIC NOT NULL)')
+con.commit()
+con.close()
